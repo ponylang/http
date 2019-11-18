8f005a7867146ab123ba4495e238d3337a3651cb
Sean T. Allen <sean@seantallen.com>
2019-11-18 14:01:57 -0500

    Move CI from CircleCI to GitHub

--- .ci-scripts/env.bash
+++
@@ -1,8 +0,0 @@
-REPO_OWNER="ponylang"
-REPO_NAME="http"
-GITHUB_USER="ponylang-main"
-
-# Who we are for git
-git config --global user.email "ponylang.main@ponylang.io"
-git config --global user.name "Main"
-git config --global push.default simple
